46b8eae755d9afd5b99ebd44dd00febd5fdc2ab7
StevenMup2004 <dang.vh225962@sis.hust.edu.vn>
2024-03-13 18:02:59 +0700

    Complete assignment 6.5

---
+++ Lab 01/ArrayOperation.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOperation {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        
+        int[] m = new int[size];
+        
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            m[i] = scanner.nextInt();
+        }
+       
+        Arrays.sort(m);
+        
+        
+        int sum = 0;
+        
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i];
+        }
+        double average = (double) sum / size;
+        
+        System.out.println("Sorted array: " + Arrays.toString(m));
+        System.out.println("Sum of array: " + sum);
+        System.out.println("Average value of array: " + average);
+        
+        scanner.close();
+    }
+}
+
