5f642d3d3ce9481107be2dc9a8c9d4c987d764d4
StevenMup2004 <dang.vh225962@sis.hust.edu.vn>
2024-04-25 17:18:35 +0700

    String, StringBuilder and StringBuffer excercise

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f7/602394d74a02001f15d2ea1d95c18a0c
@@ -0,0 +1,53 @@
+package hust.soict.globalict.aims.media;
+
+public abstract class Media {
+
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+
+    // Constructors
+    public Media() {
+    }
+
+    public Media(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
--- OtherProjects/src/hust/soict/globalict/lab01/MonthDays.java
+++ OtherProjects/src/hust/soict/globalict/lab01/MonthDays.java
@@ -11,24 +11,169 @@ public class MonthDays {
         System.out.print("Enter the year: ");
         int year = scanner.nextInt();
         switch (month) {
-            case "January", "Jan.", "Jan", "1", "March", "Mar.", "Mar", "3", "May", "5", "July", "Jul.", "Jul", "7",
-                    "August", "Aug.", "Aug", "8", "October", "Oct.", "Oct", "10", "December", "Dec.", "Dec", "12":
-                System.out.println("There are 31 days in the month");
-                break;
-            case "April", "Apr.", "Apr", "4", "June", "Jun.", "Jun", "6", "September", "Sep.", "Sep", "9", "November",
-                    "Nov.", "Nov", "11":
-                System.out.println("There are 30 days in the month");
-                break;
-            case "2", "February", "Feb.", "Feb":
-                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  
-                       System.out.println("There are 29 days in the month.");
-                       break;
-                }
-                else {
-                        System.out.println("There are 28 days in the month.");
-                        break;
-                }
-            default:
+            case "January":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Jan.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Jan":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "1":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "March":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Mar.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Mar":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "3":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "May":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "5":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "July":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Jul.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Jul":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "7":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "August":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Aug.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Aug":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "8":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "October":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Oct.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Oct":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "10":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "December":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Dec.":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "Dec":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "12":
+			System.out.println("There are 31 days in the month");
+			break;
+		case "April":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Apr.":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Apr":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "4":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "June":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Jun.":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Jun":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "6":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "September":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Sep.":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Sep":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "9":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "November":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Nov.":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "Nov":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "11":
+			System.out.println("There are 30 days in the month");
+			break;
+		case "2":
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  
+			       System.out.println("There are 29 days in the month.");
+			       break;
+			}
+			else {
+			        System.out.println("There are 28 days in the month.");
+			        break;
+			}
+		case "February":
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  
+			       System.out.println("There are 29 days in the month.");
+			       break;
+			}
+			else {
+			        System.out.println("There are 28 days in the month.");
+			        break;
+			}
+		case "Feb.":
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  
+			       System.out.println("There are 29 days in the month.");
+			       break;
+			}
+			else {
+			        System.out.println("There are 28 days in the month.");
+			        break;
+			}
+		case "Feb":
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {  
+			       System.out.println("There are 29 days in the month.");
+			       break;
+			}
+			else {
+			        System.out.println("There are 28 days in the month.");
+			        break;
+			}
+		default:
                 System.out.println("Invalid Month");
                 break;
         }
