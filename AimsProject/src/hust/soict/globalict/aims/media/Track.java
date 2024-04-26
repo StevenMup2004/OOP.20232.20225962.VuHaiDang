fe7cf3051b3be56982e4174839fc6a9c39ea62fe
StevenMup2004 <dang.vh225962@sis.hust.edu.vn>
2024-04-26 20:31:45 +0700

    Override equals method

--- AimsProject/src/hust/soict/globalict/aims/media/Track.java
+++ AimsProject/src/hust/soict/globalict/aims/media/Track.java
@@ -19,5 +19,12 @@ public class Track implements Playable{
 		System.out.println("Playing track: " + this.getTitle());
 		System.out.println("Track length: " + this.getLength());
 		}
-
+	
+	public boolean equals(Object t) {
+		if (t instanceof Track) {
+			Track T = (Track)t;
+			return (this.title.equals(T.getTitle()) && this.length == T.getLength());
+		}
+		return false;
+	}
 }
